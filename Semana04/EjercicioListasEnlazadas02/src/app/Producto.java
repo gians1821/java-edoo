5de3de7e31c098160bbeeb262d3de12deea8e870
gians1821 <productive.gian.112020@gmail.com>
2023-10-17 20:25:58 -0500

    Producto class started

--- Semana04/EjercicioListasEnlazadas02/src/app/Producto.java
+++ Semana04/EjercicioListasEnlazadas02/src/app/Producto.java
@@ -2,25 +2,58 @@ package app;
 
 public class Producto {
     
-    private int entero;
+    private String codigo;
+    private String nombre;
+    private float precio;
+    private int stock;
     private Producto sgte;
 
-    public Producto(int entero) {
-        this.entero = entero;
+    public Producto(String codigo, String nombre, float precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
         sgte = null;
     }
 
-    public Producto(int entero, Producto sgte) {
-        this.entero = entero;
+    public Producto(String codigo, String nombre, float precio, int stock, Producto sgte) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
         this.sgte = sgte;
     }
 
-    public int getEntero() {
-        return entero;
+    public String getCodigo() {
+        return codigo;
     }
 
-    public void setEntero(int entero) {
-        this.entero = entero;
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
     }
 
     public Producto getSgte() {
