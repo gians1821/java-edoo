02edc7487e2f811011d0520bd4f09f3b79a818c1
gians1821 <gian.samana.1801@gmail.com>
2024-01-07 20:13:49 -0500

    feat(semana8): resuelto ejercicio 06 de los ejercicios propuestos

--- Semana08/ejercicios-propuestos/s08-propuestos-ejercicio06/src/app/controller/Control.java
+++ Semana08/ejercicios-propuestos/s08-propuestos-ejercicio06/src/app/controller/Control.java
@@ -1,9 +1,76 @@
 package app.controller;
 
+import app.model.Pila;
+
 public class Control {
     
     public static String convertir(String infija) {
+        Pila<Character> pilaOperadores = new Pila<>();
         String posfija = "";
+        for (char car : infija.toCharArray()) {
+            System.out.println(car);
+            switch (car) {
+                case '+':
+                case '-':
+                    if (pilaOperadores.empty()) pilaOperadores.push(car);
+                    else if (pilaOperadores.top() == '(') {
+                        pilaOperadores.push(car);
+                    } else {
+                        while (!pilaOperadores.empty()) {
+                            posfija += pilaOperadores.pop();
+                        }
+                        pilaOperadores.push(car);
+                    }   break;
+                case '*':
+                case '/':
+                    if (pilaOperadores.empty()) {
+                        pilaOperadores.push(car);
+                    } else if (pilaOperadores.top() == '-' 
+                            || pilaOperadores.top() == '+'
+                            || pilaOperadores.top() == '(') {
+                        pilaOperadores.push(car);
+                    } else {
+                        while (!pilaOperadores.empty()) {
+                            posfija += pilaOperadores.pop();
+                        }
+                        pilaOperadores.push(car);
+                    }
+                    break;
+                case '^':
+                    if (pilaOperadores.empty()) {
+                        pilaOperadores.push(car);
+                    } else if (pilaOperadores.top() == '+' 
+                            || pilaOperadores.top() == '-'
+                            || pilaOperadores.top() == '*'
+                            || pilaOperadores.top() == '/'
+                            || pilaOperadores.top() == '(') {
+                        pilaOperadores.push(car);
+                    } else {
+                        while (!pilaOperadores.empty()) {
+                            posfija += pilaOperadores.pop();
+                        }
+                        pilaOperadores.push(car);
+                    }
+                    break;
+                case '(':
+                    pilaOperadores.push(car);
+                    break;
+                case ')':
+                    if (!pilaOperadores.empty()) {
+                        while (pilaOperadores.top() != '(') {
+                            posfija += pilaOperadores.pop();
+                        }
+                        pilaOperadores.pop();
+                    }
+                    break;
+                default:
+                    posfija += car;
+                    break;
+            }
+        }
+        while (!pilaOperadores.empty()) {
+            posfija += pilaOperadores.pop();
+        }
         return posfija;
     }
     
