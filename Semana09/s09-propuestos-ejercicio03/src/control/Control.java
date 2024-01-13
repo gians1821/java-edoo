72a792998f5c7b9d56f9528ba6d48477f798839b
gians1821 <gian.samana.1801@gmail.com>
2024-01-13 12:03:39 -0500

    feat(semana9): resuelto ejercicio 03

--- Semana09/s09-propuestos-ejercicio03/src/control/Control.java
+++ Semana09/s09-propuestos-ejercicio03/src/control/Control.java
@@ -2,13 +2,9 @@ package control;
 
 import javax.swing.DefaultListModel;
 import model.Cliente;
-import model.Cola;
 import model.ColaPrioridades;
 
 public class Control {
-    
-    private static Float montoTotalDeConsumo = 0f;
-    private static Integer totalAtendidos = 0;
     
     public static void encolar(ColaPrioridades<Cliente> cola, String nombre, String prioridad) {
         if (nombre.compareTo("") == 0 || prioridad.compareTo("") == 0) return;
@@ -17,14 +13,10 @@ public class Control {
         cola.insertar(cliente, cliente.getPrioridad());
     }
     
-    public static void atender(Cola<Cliente> cola) {
-        Cliente cliente = cola.desencolar();
+    public static void atender(ColaPrioridades<Cliente> cola) {
+        Cliente cliente = cola.quitarMin();
         if (cliente == null) return;
         MessageController.showInfoMessage(cliente + " ha sido atendido");
-//        montoTotalDeConsumo += cliente.getMontoCompra();
-        totalAtendidos++;
-        MessageController.showInfoMessage("En total, ya se ha consumido un total de: " 
-                + montoTotalDeConsumo + " soles, atendiendo a " + totalAtendidos + " clientes");
     }
     
     public static void mostrar(ColaPrioridades<Cliente> cola, DefaultListModel modelo) {
