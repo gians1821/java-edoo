26bca3e7aa6b8c2418b771c1321931f292a72882
gians1821 <gian.samana.1801@gmail.com>
2024-01-14 23:37:59 -0500

    feat(semana9): resuelto ejercicio bicola

--- Semana09/s09-bicola/src/control/Control.java
+++ Semana09/s09-bicola/src/control/Control.java
@@ -1,40 +1,59 @@
 package control;
 
 import javax.swing.DefaultListModel;
-import model.Cola;
+import model.Bicola;
 
 public class Control {
     
-    public static void encolar(Cola cola, String txtEntero) {
+    public static void encolarFrente(Bicola<Integer> bicola, String txtEntero) {
         if (txtEntero.compareTo("") == 0) return;
         try {
-            cola.encolar(Integer.valueOf(txtEntero));
+            bicola.encolarFrente(Integer.valueOf(txtEntero));
         } catch (NumberFormatException e) {
             System.out.println(e);
         }
     }
     
-    public static void desencolar(Cola cola) {
-        Integer entero = cola.desencolar();
+    public static void encolarFinal(Bicola<Integer> bicola, String txtEntero) {
+        if (txtEntero.compareTo("") == 0) return;
+        try {
+            bicola.encolarFinal(Integer.valueOf(txtEntero));
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public static void desencolarFrente(Bicola<Integer> bicola) {
+        Integer entero = bicola.desencolarFrente();
         if (entero != null) MessageController.showInfoMessage("Numero extraído: " + entero);
     }
     
-    public static void empty(Cola cola) {
-        if (cola.isEmpty()) MessageController.showInfoMessage("La cola está vacía");
+    public static void desencolarFinal(Bicola<Integer> bicola) {
+        Integer entero = bicola.desencolarFinal();
+        if (entero != null) MessageController.showInfoMessage("Numero extraído: " + entero);
+    }
+    
+    public static void empty(Bicola<Integer> bicola) {
+        if (bicola.empty()) MessageController.showInfoMessage("La cola está vacía");
         else MessageController.showInfoMessage("La cola NO está vacía");
     }
     
-    public static void clear(Cola cola) {
-        cola.clear();
+    public static void clear(Bicola<Integer> bicola) {
+        bicola.clear();
         MessageController.showInfoMessage("La cola ha sido despejada");
     }
     
-    public static void frente(Cola cola) {
-        Integer entero = cola.frente();
+    public static void frenteBicola(Bicola<Integer> bicola) {
+        Integer entero = bicola.frenteBicola();
         if (entero != null) MessageController.showInfoMessage("Numero en frente: " + entero);
     }
     
-    public static void mostrar(Cola cola, DefaultListModel modelo) {
+    public static void finalBicola(Bicola<Integer> bicola) {
+        Integer entero = bicola.finalBicola();
+        if (entero != null) MessageController.showInfoMessage("Numero al final: " + entero);
+    }
+    
+    public static void mostrar(Bicola<Integer> cola, DefaultListModel modelo) {
         cola.mostrar(modelo);
     }
     
--- Semana09/s09-bicola/src/view/MainFrame.java
+++ Semana09/s09-bicola/src/view/MainFrame.java
@@ -2,12 +2,12 @@ package view;
 
 import control.Control;
 import javax.swing.DefaultListModel;
-import model.Cola;
+import model.Bicola;
 
 public class MainFrame extends javax.swing.JFrame {
 
     DefaultListModel modelo = new DefaultListModel();
-    Cola cola = new Cola(5);
+    Bicola<Integer> cola = new Bicola<>();
     
     public MainFrame() {
         initComponents();
@@ -21,23 +21,26 @@ public class MainFrame extends javax.swing.JFrame {
         bg = new javax.swing.JPanel();
         txtEntero = new javax.swing.JTextField();
         btnMostrar = new javax.swing.JButton();
-        btnEncolar = new javax.swing.JButton();
-        btnDesencolar = new javax.swing.JButton();
+        btnEncolarFrente = new javax.swing.JButton();
+        btnDesencolarFrente = new javax.swing.JButton();
         btnEmpty = new javax.swing.JButton();
         btnClear = new javax.swing.JButton();
         btnFrente = new javax.swing.JButton();
         scroll1 = new javax.swing.JScrollPane();
         list1 = new javax.swing.JList<>();
+        btnEncolarFinal = new javax.swing.JButton();
+        btnDesencolarFinal = new javax.swing.JButton();
+        btnFinal = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        bg.setPreferredSize(new java.awt.Dimension(800, 700));
+        bg.setPreferredSize(new java.awt.Dimension(1000, 700));
         bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         txtEntero.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
         txtEntero.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Entero", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("SansSerif", 0, 15))); // NOI18N
         txtEntero.setPreferredSize(new java.awt.Dimension(200, 40));
-        bg.add(txtEntero, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 170, -1, -1));
+        bg.add(txtEntero, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 170, -1, -1));
 
         btnMostrar.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
         btnMostrar.setText("MOSTRAR");
@@ -47,27 +50,27 @@ public class MainFrame extends javax.swing.JFrame {
                 btnMostrarActionPerformed(evt);
             }
         });
-        bg.add(btnMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 420, -1, -1));
+        bg.add(btnMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 420, -1, -1));
 
-        btnEncolar.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
-        btnEncolar.setText("ENCOLAR");
-        btnEncolar.setPreferredSize(new java.awt.Dimension(150, 30));
-        btnEncolar.addActionListener(new java.awt.event.ActionListener() {
+        btnEncolarFrente.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
+        btnEncolarFrente.setText("ENCOLAR FRENTE");
+        btnEncolarFrente.setPreferredSize(new java.awt.Dimension(200, 30));
+        btnEncolarFrente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnEncolarActionPerformed(evt);
+                btnEncolarFrenteActionPerformed(evt);
             }
         });
-        bg.add(btnEncolar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 220, -1, -1));
+        bg.add(btnEncolarFrente, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 220, -1, -1));
 
-        btnDesencolar.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
-        btnDesencolar.setText("DESENCOLAR");
-        btnDesencolar.setPreferredSize(new java.awt.Dimension(150, 30));
-        btnDesencolar.addActionListener(new java.awt.event.ActionListener() {
+        btnDesencolarFrente.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
+        btnDesencolarFrente.setText("DESENCOLAR FRENTE");
+        btnDesencolarFrente.setPreferredSize(new java.awt.Dimension(200, 30));
+        btnDesencolarFrente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDesencolarActionPerformed(evt);
+                btnDesencolarFrenteActionPerformed(evt);
             }
         });
-        bg.add(btnDesencolar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 260, -1, -1));
+        bg.add(btnDesencolarFrente, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 260, -1, -1));
 
         btnEmpty.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
         btnEmpty.setText("EMPTY");
@@ -77,7 +80,7 @@ public class MainFrame extends javax.swing.JFrame {
                 btnEmptyActionPerformed(evt);
             }
         });
-        bg.add(btnEmpty, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 300, -1, -1));
+        bg.add(btnEmpty, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 300, -1, -1));
 
         btnClear.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
         btnClear.setText("CLEAR");
@@ -87,7 +90,7 @@ public class MainFrame extends javax.swing.JFrame {
                 btnClearActionPerformed(evt);
             }
         });
-        bg.add(btnClear, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 340, -1, -1));
+        bg.add(btnClear, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 340, -1, -1));
 
         btnFrente.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
         btnFrente.setText("FRENTE");
@@ -97,44 +100,59 @@ public class MainFrame extends javax.swing.JFrame {
                 btnFrenteActionPerformed(evt);
             }
         });
-        bg.add(btnFrente, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 380, -1, 30));
+        bg.add(btnFrente, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 380, -1, 30));
 
         scroll1.setPreferredSize(new java.awt.Dimension(150, 175));
 
         list1.setModel(modelo);
         scroll1.setViewportView(list1);
 
-        bg.add(scroll1, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 220, -1, -1));
+        bg.add(scroll1, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 190, 220, 260));
+
+        btnEncolarFinal.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
+        btnEncolarFinal.setText("ENCOLAR FINAL");
+        btnEncolarFinal.setPreferredSize(new java.awt.Dimension(200, 30));
+        btnEncolarFinal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEncolarFinalActionPerformed(evt);
+            }
+        });
+        bg.add(btnEncolarFinal, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 220, -1, -1));
+
+        btnDesencolarFinal.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
+        btnDesencolarFinal.setText("DESENCOLAR FINAL");
+        btnDesencolarFinal.setPreferredSize(new java.awt.Dimension(200, 30));
+        btnDesencolarFinal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDesencolarFinalActionPerformed(evt);
+            }
+        });
+        bg.add(btnDesencolarFinal, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 260, -1, -1));
+
+        btnFinal.setFont(new java.awt.Font("SansSerif", 0, 15)); // NOI18N
+        btnFinal.setText("FINAL");
+        btnFinal.setPreferredSize(new java.awt.Dimension(150, 30));
+        btnFinal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnFinalActionPerformed(evt);
+            }
+        });
+        bg.add(btnFinal, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 380, -1, 30));
 
         getContentPane().add(bg, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnEncolarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncolarActionPerformed
-        System.out.println("ANTES");
-        System.out.println(cola.getIndexPrimero());
-        System.out.println(cola.getIndexUltimo());        
-        System.out.println("======================");
-        Control.encolar(cola, txtEntero.getText());
+    private void btnEncolarFrenteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncolarFrenteActionPerformed
+        Control.encolarFrente(cola, txtEntero.getText());
         Control.mostrar(cola, modelo);
-        System.out.println("DESPUES");
-        System.out.println(cola.getIndexPrimero());
-        System.out.println(cola.getIndexUltimo());
+    }//GEN-LAST:event_btnEncolarFrenteActionPerformed
 
-    }//GEN-LAST:event_btnEncolarActionPerformed
-
-    private void btnDesencolarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDesencolarActionPerformed
-        System.out.println("ANTES");
-        System.out.println(cola.getIndexPrimero());
-        System.out.println(cola.getIndexUltimo());        
-        System.out.println("======================");
-        Control.desencolar(cola);
+    private void btnDesencolarFrenteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDesencolarFrenteActionPerformed
+        Control.desencolarFrente(cola);
         Control.mostrar(cola, modelo);
-        System.out.println("DESPUES");
-        System.out.println(cola.getIndexPrimero());
-        System.out.println(cola.getIndexUltimo());
-    }//GEN-LAST:event_btnDesencolarActionPerformed
+    }//GEN-LAST:event_btnDesencolarFrenteActionPerformed
 
     private void btnEmptyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEmptyActionPerformed
         Control.empty(cola);
@@ -146,19 +164,36 @@ public class MainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_btnClearActionPerformed
 
     private void btnFrenteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFrenteActionPerformed
-        Control.frente(cola);
+        Control.frenteBicola(cola);
     }//GEN-LAST:event_btnFrenteActionPerformed
 
     private void btnMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMostrarActionPerformed
         Control.mostrar(cola, modelo);
     }//GEN-LAST:event_btnMostrarActionPerformed
 
+    private void btnEncolarFinalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEncolarFinalActionPerformed
+        Control.encolarFinal(cola, txtEntero.getText());
+        Control.mostrar(cola, modelo);
+    }//GEN-LAST:event_btnEncolarFinalActionPerformed
+
+    private void btnDesencolarFinalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDesencolarFinalActionPerformed
+        Control.desencolarFinal(cola);
+        Control.mostrar(cola, modelo);
+    }//GEN-LAST:event_btnDesencolarFinalActionPerformed
+
+    private void btnFinalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFinalActionPerformed
+        Control.finalBicola(cola);
+    }//GEN-LAST:event_btnFinalActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bg;
     private javax.swing.JButton btnClear;
-    private javax.swing.JButton btnDesencolar;
+    private javax.swing.JButton btnDesencolarFinal;
+    private javax.swing.JButton btnDesencolarFrente;
     private javax.swing.JButton btnEmpty;
-    private javax.swing.JButton btnEncolar;
+    private javax.swing.JButton btnEncolarFinal;
+    private javax.swing.JButton btnEncolarFrente;
+    private javax.swing.JButton btnFinal;
     private javax.swing.JButton btnFrente;
     private javax.swing.JButton btnMostrar;
     private javax.swing.JList<String> list1;
