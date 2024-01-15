2aba80a7f6dfb862a6afbbf1541596a57336dcf6
gians1821 <gian.samana.1801@gmail.com>
2024-01-14 22:10:06 -0500

    feat(semana9): created array queue model

--- Semana09/s09-propuestos-ejercicio04/src/model/Cola.java
+++ Semana09/s09-propuestos-ejercicio04/src/model/Cola.java
@@ -2,69 +2,66 @@ package model;
 
 import javax.swing.DefaultListModel;
 
-public class Cola<T> {
+public class Cola {
     
-    private Nodo<T> primero;
-    private Nodo<T> ultimo;
-
-    public Cola() {
-        primero = null;
-        ultimo = null;
+    private int maxTamaño;
+    private final Integer[] cola;
+    private int indexPrimero;
+    private int indexUltimo;
+    
+    private int siguiente(int r) {
+        return (r + 1) % maxTamaño;
     }
 
-    protected Nodo<T> getPrimero() {
-        return primero;
+    public Cola(int maxTamaño) {
+        this.maxTamaño = maxTamaño;
+        cola = new Integer[this.maxTamaño];
+        indexPrimero = 0;
+        indexUltimo = this.maxTamaño - 1;
     }
 
-    protected Nodo<T> getUltimo() {
-        return ultimo;
+    protected int getIndexPrimero() {
+        return indexPrimero;
+    }
+
+    protected int getIndexUltimo() {
+        return indexUltimo;
     }
     
-    public boolean empty() {
-        return primero == null;
+    public boolean isEmpty() {
+        return indexPrimero == siguiente(indexUltimo);
+    }
+    
+    public boolean isFull() {
+        return indexPrimero == siguiente(siguiente(indexUltimo));
     }
 
-    public void encolar(T info) {
-        Nodo<T> nuevo = new Nodo(info);
-        if (empty()) {
-            primero = nuevo;
-            ultimo = nuevo;
-        } else {
-            ultimo.setSgte(nuevo);
-            ultimo = nuevo;
+    public void encolar(Integer entero) {
+        if (!isFull()) {
+            indexUltimo = siguiente(indexUltimo);
+            cola[indexUltimo] = entero;
         }
     }
     
-    public T desencolar() {
-        if (empty()) return null;
-        Nodo<T> nodo = primero;
-        if (nodo == ultimo) {
-            primero = null;
-            ultimo = null;
-        } else {
-            primero = primero.getSgte();
-        }
-        return nodo.getInfo();
+    public Integer desencolar() {
+        if (!isEmpty()) return cola[indexPrimero++];
+        return null;
     }
     
-    public T frente() {
-        if (empty()) return null;
-        return primero.getInfo();
+    public Integer frente() {
+        if (!isEmpty()) return cola[indexPrimero];
+        return null;
     }
     
     public void clear() {
-        while (primero != null) {
-            primero = primero.getSgte();
-        }
-        ultimo = null;
+        indexPrimero = 0;
+        indexUltimo = maxTamaño - 1;
     }
     
     public void mostrar(DefaultListModel modelo) {
         modelo.removeAllElements();
-        Nodo p = primero;
-        while (p != null) {
-            modelo.addElement(p.getInfo());
-            p = p.getSgte();
+        for (int i = 0; i < maxTamaño; i++) {
+            modelo.addElement(cola[i]);
         }
     }
     
